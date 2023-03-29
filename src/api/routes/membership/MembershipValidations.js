eba0f98d9845ddfbeb894e04f200452fd6abb827
manish <mkwebsite>
2023-03-29 11:40:23 +0530

    updated code

--- src/api/routes/membership/MembershipValidations.js
+++ src/api/routes/membership/MembershipValidations.js
@@ -5,29 +5,24 @@ const create = Joi.object().keys({
     name: Joi.string() .required(),
     description: Joi.string() .required(),
     duration: Joi.date() .required(),
-
-    features: Joi.string() .required(),
-
+    features: Joi.array() .required(),
     price: Joi.number() .required(),
     bidding: Joi.number() .required(),
-    featured_membership: Joi.number() .required(),
-
-    });
+   
+    singup_features: Joi.array() .required(),
+   });
     
-    
-
 
      const updateMembership = Joi.object().keys({
         name: Joi.string() .optional(),
         description: Joi.string() .optional(),
         duration: Joi.date() .optional(),
-    
-        features: Joi.string() .optional(),
-    
+        features: Joi.array() .optional(),
         price: Joi.number() .optional(),
         bidding: Joi.number() .optional(),
-        featured_membership: Joi.number() .optional(),
-    
+        
+        singup_features: Joi.array() .optional(),
+
 
      });
 

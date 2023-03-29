7363ec0958f8e5578a0bf54bdc15b2a1558e04fe
manish <mkwebsite>
2023-03-29 11:27:15 +0530

    updated code

--- src/api/routes/membership/MembershipValidations.js
+++ src/api/routes/membership/MembershipValidations.js
@@ -2,19 +2,36 @@ const { Joi, common } = require('../../util/validations');
 const { languages } = require('../../../../lib/i18n');
 
 const create = Joi.object().keys({
-    name: Joi.string()
-    .required(),
+    name: Joi.string() .required(),
+    description: Joi.string() .required(),
+    duration: Joi.date() .required(),
+
+    features: Joi.string() .required(),
+
+    price: Joi.number() .required(),
+    bidding: Joi.number() .required(),
+    featured_membership: Joi.number() .required(),
+
     });
+    
+    
+
 
      const updateMembership = Joi.object().keys({
-        name: Joi.string()
-    .optional(),
+        name: Joi.string() .optional(),
+        description: Joi.string() .optional(),
+        duration: Joi.date() .optional(),
+    
+        features: Joi.string() .optional(),
+    
+        price: Joi.number() .optional(),
+        bidding: Joi.number() .optional(),
+        featured_membership: Joi.number() .optional(),
+    
+
      });
 
-
-
 module.exports = {
-    // requireId,
        create,
        updateMembership,
     

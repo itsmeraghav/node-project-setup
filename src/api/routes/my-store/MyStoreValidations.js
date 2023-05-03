67700023f13760cce46a598f16c95511048690a8
manish <mkwebsite>
2023-05-03 13:03:25 +0530

    changes

--- src/api/routes/my-store/MyStoreValidations.js
+++ src/api/routes/my-store/MyStoreValidations.js
@@ -1,52 +1,52 @@
-const { Joi, common } = require('../../util/validations');
-const { languages } = require('../../../../lib/i18n');
+// const { Joi, common } = require('../../util/validations');
+// const { languages } = require('../../../../lib/i18n');
 
 
 
-const createtype = Joi.object().keys({
+// const createtype = Joi.object().keys({
  
-    search_product: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-    .required(),
-        description: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .required(),
-        product_title: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .required(),
-        ingredients: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .required(),
-        tags: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .required(),
-        spice_level: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .required(),
-        type: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .required(),
-        product_photos: Joi.string()
-        .required(),
-});
+//     search_product: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//     .required(),
+//         description: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .required(),
+//         product_title: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .required(),
+//         ingredients: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .required(),
+//         tags: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .required(),
+//         spice_level: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .required(),
+//         type: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .required(),
+//         product_photos: Joi.string()
+//         .required(),
+// });
 
 
-const updateProfile = Joi.object().keys({
-    search_product: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .optional(),
-        description: Joi.number()
-        .optional(),
-        product_title: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .optional(),
-        ingredients: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .optional(),
-        tags: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .optional(),
-        spice_level: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .optional(),
-        type: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-        .optional(),
-        product_photos: Joi.string()
-        .optional(),
-});
+// const updateProfile = Joi.object().keys({
+//     search_product: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .optional(),
+//         description: Joi.number()
+//         .optional(),
+//         product_title: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .optional(),
+//         ingredients: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .optional(),
+//         tags: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .optional(),
+//         spice_level: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .optional(),
+//         type: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//         .optional(),
+//         product_photos: Joi.string()
+//         .optional(),
+// });
 
 
-module.exports = {
+// module.exports = {
 
-    createtype,
-    updateProfile
+//     createtype,
+//     updateProfile
 
-};
+// };

54d588f65dfa667d2da5df205aa9b5cd2f2afb7c
manish <mkwebsite>
2023-05-09 17:23:07 +0530

    faq

--- src/api/routes/faq/FAQValidations.js
+++ src/api/routes/faq/FAQValidations.js
@@ -5,8 +5,12 @@ const { languages } = require('../../../../lib/i18n');
 
 const createservice = Joi.object().keys({
     
-    service_name: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-    .required(),
+    // service_name: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+    // .required(),
+    question: Joi.string()
+    .optional(),
+    answer: Joi.string()
+    .optional(),
      
        
 });
@@ -15,8 +19,8 @@ const createservice = Joi.object().keys({
 
 
 const updateProfile = Joi.object().keys({
-    service_name: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
-    .optional(),
+//     service_name: Joi.string().regex(/^[a-zA-Z][a-zA-Z ]*$/)
+//     .optional(),
 });
 
 
